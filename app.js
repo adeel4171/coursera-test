0f4c4a872573840b34c193f54597310719c2632c
adeel4171 <adeel7386@gmail.com>
2017-01-18 15:58:43 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,16 @@
+var app=angular.module('LunchChecker',[]);
+app.controller('LunchCheckController',function($scope){
+	$scope.list="HELLO"
+
+	$scope.CheckList = function(){
+		$scope.message="Please Enter Data First!"
+		var splits = $scope.list.split(',');
+		if (splits.length <= 3){
+			$scope.message="Enjoy!";
+		}
+		else
+			$scope.message="Too Much!";
+
+
+	}
+});
